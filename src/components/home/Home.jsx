91688ece6ad1ca90278fb024ac7fcb33f7f5c1f5
ItamarGouveia <contato.itamargouveia@gmail.com>
2025-06-21 18:47:18 -0300

    ff

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import profileImg from "../../assets/profile-img.png";
+import profileImg from "../../assets/profile2-img.png";
 import shapeOne from "../../assets/shape-1.png";
 import shapeTwo from "../../assets/shape-2.png";
 import "./Home.css";
@@ -10,15 +10,45 @@ const Home = () => {
     <section className="home" id="home">
       <div className="home__container container">
         <p className="home__subtitle text-cs">
-          Hello, <span>My name is</span>
+          Olá, <span>Meu nome é</span>
         </p>
         <h1 className="home__title text-cs">
-          <span>ITAMAR</span> GOUVEIA
+          <span>ITAMAR</span> <br />
+          GOUVEIA
         </h1>
         <p className="home__job">
-          <span className="text-cs">I am </span>
-          <b>Web Developer</b>
+          <span className="text-cs">Eu sou um </span>
+          <b>Desenvolvedor Web</b>
         </p>
+
+        <div className="home__img-wrapper">
+          <div className="home__banner">
+            <img src={profileImg} alt="" className="home__profile" />
+          </div>
+
+          <p className="home__data home__data-one">
+            <span className="text-lg">
+              12 <b>+</b>
+            </span>
+
+            <span className="text-sm text-cs">
+              Anos de <span>Experiência</span>
+            </span>
+          </p>
+
+          <p className="home__data home__data-two">
+            <span className="text-lg">30</span>
+
+            <span className="text-sm text-cs">
+              Projetos <span>completados</span>
+            </span>
+          </p>
+
+          <img src={shapeOne} alt="" className="shape shape__1" />
+          <img src={shapeTwo} alt="" className="shape shape__2" />
+          <img src={shapeOne} alt="" className="shape shape__3" />
+        </div>
+
         <p className="home__text">
           Sou um desenvolvedor full stack com paixão por transformar ideias em
           soluções funcionais. Tenho experiência tanto no desenvolvimento de
